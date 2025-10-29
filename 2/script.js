a36da357ff4a40101a6c9fee688c9daced003f1f
saddam848811 <sh848811@gmail.com>
2025-10-29 17:00:26 +0530

    first commit

---
+++ 2/script.js
@@ -0,0 +1,9 @@
+    const div = document.getElementById("con");
+
+    div.addEventListener("mouseenter", function() {
+        div.style.backgroundColor = "skyblue";
+    });
+
+    div.addEventListener("mouseleave", function() {
+        div.style.backgroundColor = "lightgray";
+    });
